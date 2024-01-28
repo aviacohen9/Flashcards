428cb19ea4bf85dabbf722c91e6dac977ab6a962
aviachn <cohen.avia9@gmail.com>
2024-01-28 23:41:33 +0200

    adding href to an a tag

--- frontend/src/components/Alert.js
+++ frontend/src/components/Alert.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-//import { Link } from "react-router-dom";
 
 //style
 import '../styling/App.css'
@@ -15,24 +14,17 @@ const Alert = ({ variant }) => {
           border: "0.1rem solid " + variant.secondaryColor,
         }}
       >
-        <div
-          className="symbol-container"
-          style={{ background: variant.secondaryColor }}
-        >
-          <span className="material-symbols-outlined symbol">{variant.symbol}</span>{" "}
-        </div>
-        <div className="description-container">
-          <span className="description-text"  >{variant.text}</span>
-        </div>
-        
-        {/*
-        <div className="buttons_options">
-            <button><Link to="/">Homepage</Link></button>
-        </div>
-         */}
-        <a className="symbol-close-link" onClick={() => setOpen(false)}>
-        <span className="material-symbols-outlined symbol" style={{color: variant.secondaryColor}}>cancel</span>{" "}
-        </a>
+            <div className="symbol-container" style={{ background: variant.secondaryColor }}>
+                <span className="material-symbols-outlined symbol">{variant.symbol}</span>{" "}
+            </div>
+
+            <div className="description-container">
+                <span className="description-text"  >{variant.text}</span>
+            </div>
+
+            <a className="symbol-close-link" href="/#" onClick={() => setOpen(false)}>
+                <span className="material-symbols-outlined symbol" style={{color: variant.secondaryColor}}>cancel</span>{" "}
+            </a>
         </div>
     );
 };
